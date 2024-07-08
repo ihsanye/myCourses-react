74d3bd6b49c94e2af53136be6b960fd3f7dd345f
ihsanye <ihsany98@gmail.com>
2024-07-08 23:40:10 +0300

    courses info added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
+import { courses } from './Data'
 
 function App() {
 
---
+++ src/Data.jsx
@@ -0,0 +1,43 @@
+import Csharp from './images/csharpCourse.jpg'
+import Jscript from './images/jsCourse.jpg'
+import Python from './images/pythonCourse.jpg'
+import Reactjs from './images/reactCourse.jpg'
+
+export const courses = [
+    {
+        "id": "1",
+        "title": "Javascript",
+        "description": "Javascript ogrenmesi kolay, web gelistirmenin hem on hem arka yuzunde kullanilan cok efektif ve populer bir yazilim dilidir",
+        "price": "200 TL",
+        "link": "https://www.google.com/",
+        "img": Jscript
+    },
+    {
+        "id": "2",
+        "title": "Python",
+        "description": "Python ogrenmesi kolay; web gelistirme, veri bilimi, yapay zeka gibi bir cok alanda kullanilan populer bir yazilim dilidir",
+        "price": "200 TL",
+        "link": "https://www.google.com/",
+        "img": Python
+
+    },
+    {
+        "id": "3",
+        "title": "C#",
+        "description": "C# Microsoft tarafindan gelistirilmis bircok alanda kullanilan cok guclu bir yazilim dilidir",
+        "price": "200 TL",
+        "link": "https://www.google.com/",
+        "img": Csharp
+
+    },
+    {
+        "id": "4",
+        "title": "React",
+        "description": "Javascript kutuphanesidir, Facebook tarafindan gelistirilmistir",
+        "price": "200 TL",
+        "link": "https://www.google.com/",
+        "img": Reactjs
+
+    }
+
+]
